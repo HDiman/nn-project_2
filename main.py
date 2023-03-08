e1855e2c0bc8799d7f31f85d7162dfb051d03713
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-08 18:28:20 +0300

    Работает Версия 1.6 (автоматизирована) if __name__ == "__main__":     ...

--- main.py
+++ main.py
@@ -23,75 +23,76 @@ stock_num = int(input("Сколько акций купить? "))
 bond_num = int(input("Сколько облигаций купить? "))
 clear()
 
-# Market Prices
-for month in range(time_horizon):
+if __name__ == "__main__":
+    # Market Prices
+    for month in range(time_horizon):
 
-    # Цена акции
-    monthly_return = math.exp(random.gauss(0.0, volatility)) - 1.0
-    stock_price *= 1.0 + monthly_return
+        # Цена акции
+        monthly_return = math.exp(random.gauss(0.0, volatility)) - 1.0
+        stock_price *= 1.0 + monthly_return
 
-    # Форс Мажор происходит раз в 4 года
-    force_majeure = random.randint(1, 48)
-    if force_majeure == 24:
-        print("Падение -50%")
-        stock_price = stock_price / 2
-    elif force_majeure == 12:
-        print("Рост +100%")
-        stock_price = stock_price * 2
-    stock_price = round(stock_price, 2)
+        # Форс Мажор происходит раз в 4 года
+        force_majeure = random.randint(1, 48)
+        if force_majeure == 24:
+            print("Падение -50%")
+            stock_price = stock_price / 2
+        elif force_majeure == 12:
+            print("Рост +100%")
+            stock_price = stock_price * 2
+        stock_price = round(stock_price, 2)
 
-    # Цена облигации вместе с процентами
-    bond_price = round((bond_price + 6), 2)  # пересчитать калькуляцию !!!
+        # Цена облигации вместе с процентами
+        bond_price = round((bond_price + 6), 2)  # пересчитать калькуляцию !!!
 
-    # Общая сумма налички
-    cash += 10000
+        # Общая сумма налички
+        cash += 10000
 
-    # Блок оценки стоимости портфеля
-    stock_case = round((stock_price * stock_num), 2)
-    bond_case = round((bond_price * bond_num), 2)
-    personal_case = round((stock_case + bond_case + cash), 2)
-    stock_interest = round(stock_case / (personal_case / 100))
-    bond_interest = 100 - stock_interest
-    start_capital += 10000
+        # Блок оценки стоимости портфеля
+        stock_case = round((stock_price * stock_num), 2)
+        bond_case = round((bond_price * bond_num), 2)
+        personal_case = round((stock_case + bond_case + cash), 2)
+        stock_interest = round(stock_case / (personal_case / 100))
+        bond_interest = 100 - stock_interest
+        start_capital += 10000
 
-    # Блок вывода информации портфеля
-    print(f"Месяц: {month+1}")
-    print("")
-    print(f"Цена акции: {stock_price}")
-    print(f"У вас: {stock_num} акций")
-    print(f"Капитал в акциях: {stock_case} руб.")
-    print(f"Капитал в акциях: {stock_interest} %")
-    print("")
-    print(f"У вас: {bond_num} облигаций")
-    print(f"Капитал в облигациях: {bond_case} руб.")
-    print(f"Капитал в облигациях: {bond_interest} %")
-    print("")
-    print(f"Капитал в наличке: {cash} руб.")
-    print("")
-    print(f"Всего у вас: {personal_case} руб.")
-    print(f"Всего внесено: {start_capital} руб.")
+        # Блок вывода информации портфеля
+        print(f"Месяц: {month+1}")
+        print("")
+        print(f"Цена акции: {stock_price}")
+        print(f"У вас: {stock_num} акций")
+        print(f"Капитал в акциях: {stock_case} руб.")
+        print(f"Капитал в акциях: {stock_interest} %")
+        print("")
+        print(f"У вас: {bond_num} облигаций")
+        print(f"Капитал в облигациях: {bond_case} руб.")
+        print(f"Капитал в облигациях: {bond_interest} %")
+        print("")
+        print(f"Капитал в наличке: {cash} руб.")
+        print("")
+        print(f"Всего у вас: {personal_case} руб.")
+        print(f"Всего внесено: {start_capital} руб.")
 
-    # Блок уравнивания
-    # answer = input("Уравнять 50% на 50% (Да - a, Нет - s): ")
-    if stock_interest >= 70 or bond_interest >= 70 or cash == 50000:
-        first_part = round(personal_case) / 2
-        second_part = round(personal_case) - first_part
-        stock_num = round(first_part / stock_price)
-        bond_price = 1000
-        bond_num = round(second_part / bond_price)
-        cash = 0
+        # Блок уравнивания
+        # answer = input("Уравнять 50% на 50% (Да - a, Нет - s): ")
+        if stock_interest >= 70 or bond_interest >= 70 or cash == 50000:
+            first_part = round(personal_case) / 2
+            second_part = round(personal_case) - first_part
+            stock_num = round(first_part / stock_price)
+            bond_price = 1000
+            bond_num = round(second_part / bond_price)
+            cash = 0
 
-    # Доработать
-    # if cash == 30000:
-    #     print("Докупаем облигации на 30 тыс. руб.")
-    #     bond_sum = bond_case + cash
-    #     bond_price = 1000
-    #     bond_num = round(bond_sum / bond_price)
-    #     cash = 0
+        # Доработать
+        # if cash == 30000:
+        #     print("Докупаем облигации на 30 тыс. руб.")
+        #     bond_sum = bond_case + cash
+        #     bond_price = 1000
+        #     bond_num = round(bond_sum / bond_price)
+        #     cash = 0
 
-    time.sleep(3)
-    if month < 119:
-        clear()
+        time.sleep(3)
+        if month < 119:
+            clear()
 
 
 
